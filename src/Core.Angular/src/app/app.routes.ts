a124dd3a53ffcf5d952b22a0653d97126d0f2fc3
Fernando Goncalves Jose <fernando.jose@corp.nova>
2018-04-24 18:17:49 -0300

    alteração da pasta do security

--- src/Core.Angular/src/app/app.routes.ts
+++ src/Core.Angular/src/app/app.routes.ts
@@ -2,7 +2,7 @@ import { LoginComponent } from './components/login/login.component';
 import { HomeComponent } from './components/home/home.component';
 import { Routes, RouterModule } from '@angular/router';
 import { ModuleWithProviders } from '@angular/core';
-import { AuthGuard } from './components/security/auth.guard';
+import { AuthGuard } from './security/auth.guard';
 import { UserNewComponent } from './components/user-new/user-new.component';
 import { UserGameComponent } from './components/user-game/user-game.component';
 import { OficialGameComponent } from './components/oficial-game/oficial-game.component';
