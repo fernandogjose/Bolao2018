4a44721727714232b836807b30c27ffc92221a7a
Fernando Goncalves Jose <fernando.jose@corp.nova>
2018-05-17 16:39:03 -0300

    string

--- src/Core.Data/Repositories/BaseRepository.cs
+++ src/Core.Data/Repositories/BaseRepository.cs
@@ -3,7 +3,7 @@ using System;
 namespace Core.Data.Repositories {
     public class BaseRepository {
         public string ConnectionString () {
-            return "Data Source=sql4.porta80.com.br;Initial Catalog=ecoaprender;Integrated Security=False;User ID=ecoaprender;Password=ahv5G2pH4a@;MultipleActiveResultSets=True;Connect Timeout=30;Encrypt=True;TrustServerCertificate=True";
+            return "";
         }
 
         public string GetDbValue (string value) {
