40c7b0ac2be520b8e2d9023be8861c4bbb65c20a
unknown <fernandogjose@gmail.com>
2018-05-17 08:14:59 -0300

    inclusao das novas formas de pagamento

--- src/Core.Angular/src/app/services/config.service.ts
+++ src/Core.Angular/src/app/services/config.service.ts
@@ -1,3 +1,4 @@
-export const  BASE_URL_API: string = 'https://bolao-api.azurewebsites.net/api';
+// export const  BASE_URL_API: string = 'https://bolao-api.azurewebsites.net/api';
 // export const  BASE_URL_API: string = 'http://localhost:5000/api';
-// export const  BASE_URL_API: string = 'http://copadomundobolao.com.br//api//api';
+export const  BASE_URL_API: string = 'http://copadomundobolao.com.br/api/api';
+// export const  BASE_URL_API: string = 'http://fernandogjose-001-site1.dtempurl.com/api';
