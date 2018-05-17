2f2eeda4a48feac88ce6d9f23fad35dbc899ca7a
Fernando Goncalves Jose <fernando.jose@corp.nova>
2018-05-17 16:40:27 -0300

    inclusão do gitignore

--- src/Core.Data/Repositories/BaseRepository.cs
+++ src/Core.Data/Repositories/BaseRepository.cs
@@ -3,7 +3,7 @@ using System;
 namespace Core.Data.Repositories {
     public class BaseRepository {
         public string ConnectionString () {
-            return "";
+            return "Data Source=sql4.porta80.com.br;Initial Catalog=ecoaprender;Integrated Security=False;User ID=ecoaprender;Password=;MultipleActiveResultSets=True;Connect Timeout=30;Encrypt=True;TrustServerCertificate=True";
         }
 
         public string GetDbValue (string value) {
